af55848c6da8487784df1f8bef0a178ed13ebe58
tinkugupta5 <tinku.gupta4@gmail.com>
2023-05-25 22:57:21 +0530

    adding navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,11 @@ import './Header.css'
 
 const Header = () => {
   return (
+    <>
     <section className="h-wrapper">
-      <div className="flexCenter paddings h-container">
+      <div className="flexCenter innerWidth paddings h-container">
         <img src="./logo.png" alt="" width={100} />
-        <div className="h-menu">
+        <div className="h-menu flexCenter">
           <a href="">Residencies</a>
           <a href="">Our Value</a>
           <a href="">Contact Us</a>
@@ -17,7 +18,9 @@ const Header = () => {
           
         </div>
       </div>
+
     </section>
+</>
   );
 };
 
