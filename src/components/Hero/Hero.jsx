28f85cb307b1e1c9b83de4dc2fa824f18a640262
tinkugupta5 <tinku.gupta4@gmail.com>
2023-05-27 02:09:24 +0530

    adding button

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <Section className="hero-wrapper">
+      <div className="paddings innerWidth flexCenter hero-container ">
+
+      <div className='hero-left'>
+        Left Section
+      </div>
+     <div className='flexCenter hero-left'>
+        <div className="image-container">
+          <img src='./hero-image.png' alt=''/>
+        </div>
+     </div>
+     
+      </div>
+    </Section>
   )
 }
 
