7c7a789cef5348e8df3e3762007fe90318ff3473
tinkugupta5 <tinku.gupta4@gmail.com>
2023-06-08 23:20:18 +0530

    adding accordian

--- src/Value/Value.jsx
+++ src/Value/Value.jsx
@@ -5,11 +5,11 @@ import {
   AccordionItemHeading,
   AccordionItemButton,
   AccordionItemPanel,
-  AccordionItemState,
-} from "react-accessible-accordion";
-import "react-accessible-accordion/dist/fancy-example.css";
+} from 'react-accessible-accordion';
+
 import data from '../utils/accordion'
 import { MdOutlineArrowDropDown } from "react-icons/md";
+import 'react-accessible-accordion/dist/fancy-example.css';
 const Value = () => {
   return (
     <section className="v-wrapper">
@@ -31,24 +31,34 @@ const Value = () => {
           </span>
 
           <Accordion
-            className="accordion-container"
+            className="accordion"
             allowMultipleExpanded={false}
             preExpanded={[0]}>
-                {
-                    data.map((item,i)=>{
-                        return
-                        (
+                
+                    {data.map((item,i) => {
+                        return(                        
                             <AccordionItem className="accordionItem" key={i} uuid={i}>
                                 <AccordionItemHeading>
                                   <AccordionItemButton>
                                     <div className="flexCenter icon">{item.icon}</div>
+                                    <span className="primaryText">
+                                      {item.heading}
+                                    </span>
+                                    <div className="flexCenter icon">
+                                      <MdOutlineArrowDropDown size={20}/>
+                                    </div>
                                   </AccordionItemButton>
                                 </AccordionItemHeading>
+                                <AccordionItemPanel>
+                                  <p className="secondaryText">{item.detail}</p>
+                                </AccordionItemPanel>
                             </AccordionItem>
                         )
                     })
                 }
           </Accordion>
+
+         
         </div>
       </div>
     </section>
